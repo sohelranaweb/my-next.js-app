695b30e3f84d4352edb511864a8781dce96e8da6
sohelranaweb <sohel.rana.web2@gmail.com>
2026-03-26 12:47:20 +0600

    for image add in readme file

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,7 +6,9 @@ function Counter() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white shadow-md rounded-2xl p-6 text-center">
-        <h1 className="text-xl text-gray-400 font-bold mb-4">Simple Counter</h1>
+        <h1 className="text-xl text-gray-400 font-bold mb-4">
+          Simple Counter for Server Side Test
+        </h1>
         <p className="text-4xl font-semibold text-indigo-600 mb-6">{count}</p>
         <div className="flex gap-3">
           <div>
